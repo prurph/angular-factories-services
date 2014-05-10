bbccd6017866bc8e4fc3a81299d6d6899bde88f1
Prescott Murphy <prurph@gmail.com>
2014-05-10 11:49:31 -0400

    Scoring, add/remove players

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var app = angular.module('dribbleScorer', []);
+/* jshint camelcase: false */
 
-app.controller('DribbleController', function($scope) {
+'use strict';
+
+var app = angular.module('dribbbleScorer', []);
+
+app.controller('DribbbleController', function($scope, $http) {
   $scope.newPlayer = null;
-  $scope.players = ['Tom', 'Dick', 'Harry'];
+  $scope.players = [];
 
   $scope.addPlayer = function(player) {
-    $scope.players.push(player);
+    $http.jsonp(
+      'http://api.dribbble.com/players/' + player + '?callback=JSON_CALLBACK'
+    ).success(function(dribbble_player) {
+      $scope.players.push(dribbble_player);
+    }
+    ).error(function(){
+      // handle errors
+    });
+  };
+
+  $scope.removePlayer = function(player) {
+    $scope.players.splice($scope.players.indexOf(player), 1);
+  };
+
+  $scope.likeScore = function(player) {
+    return player.likes_received_count - player.likes_count;
+  };
+
+  $scope.commentScore = function(player) {
+    return player.comments_received_count - player.comments_count;
   };
 });
